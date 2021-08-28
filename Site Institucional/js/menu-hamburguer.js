71c95d969ee7778084c6348adc59a516cdccc422
malevia10 <matheus.vsantos@bandtec.com.br>
2021-08-28 20:27:12 -0300

    Menu hamburguer e parte do index.html

--- Site Institucional/js/menu-hamburguer.js
+++ Site Institucional/js/menu-hamburguer.js
@@ -1,28 +1,28 @@
-class MenuHamburguer {
-    constructor(MenuHamburguer, navList, navSelector) {
-        this.mobileMenu = document.querySelector(mobileMenu);
+class MobileNavbar {
+    constructor(MenuHamburguer, navList, navLinks) {
+        this.MenuHamburguer = document.querySelector(MenuHamburguer);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
     }
 
     addClickEvent() {
-        this.mobileMenu.addEventListener("click", () => console.log
+        this.MenuHamburguer.addEventListener("click", () => console.log
         ("Hey"));
     }
 
     init() {
-        if(this.mobileMenu) {
+        if(this.MenuHamburguer) {
             this.addClickEvent();
         }
 
         return this;
     }
-
-    const mobileNavbar = new MobileNavbar(
-        "mobile-menu",
-        "nav-list",
-        "nav-list li"
-    )
-
 }
+
+const mobileNavBar = new MobileNavbar(
+    "menu-hamburguer",
+    "nav-list",
+    "nav-list li"
+);
+mobileNavBar.init();
