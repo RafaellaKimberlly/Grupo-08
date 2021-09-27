c22a32391d6404a5e9a14a22d8b74e5b94903c9f
ReyelZeras <reyel.conceicao@bandtec.com.br>
2021-09-27 10:16:19 -0300

    Ajustes na API

---
+++ Site Institucional/WebSite/models/leitura.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Leitura = sequelize.define('Leitura',{	
+		id: {
+			field: 'idPaciente',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},	
+		temperatura: {
+			field: 'temperatura',
+			type: DataTypes.REAL,
+			allowNull: false
+		},
+		
+		momento: {
+			field: 'momento',
+			defaultValue: data(),
+			type: DataTypes.DATE, // NÃO existe DATETIME. O tipo DATE aqui já tem data e hora
+			allowNull: false
+		},
+		fkPaciente: {
+			field: 'fkPaciente',
+			type: DataTypes.INTEGER, // NÃO existe DATETIME. O tipo DATE aqui já tem data e hora
+			allowNull: false
+		},
+		momento_grafico: {
+			type: DataTypes.VIRTUAL, // campo 'falso' (não existe na tabela). Deverá ser preenchido 'manualmente' no select
+			allowNull: true
+		}
+	}, 
+	{
+		tableName: 'leitura', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Leitura;
+};
+
+
+function data(){
+	var dataAtual = new Date();
+	dataAtual.setHours(dataAtual.getHours()-3)
+	return dataAtual;
+}
