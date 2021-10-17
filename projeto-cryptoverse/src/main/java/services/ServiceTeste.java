b7dbdd75042fc5cd20152bc79aa4820606e5ed13
Zakeh1480 <zakeh0875@hotmail.com>
2021-10-17 16:20:35 -0300

    Criação do service e testes na main(application)

--- projeto-cryptoverse/src/main/java/services/ServiceTeste.java
+++ projeto-cryptoverse/src/main/java/services/ServiceTeste.java
@@ -6,37 +6,50 @@ import com.github.britooo.looca.api.group.memoria.Memoria;
 import com.github.britooo.looca.api.group.processador.Processador;
 
 public class ServiceTeste {
-    
+
     Memoria ram = new Memoria();
     Processador processador = new Processador();
     DiscosGroup disco = new DiscosGroup();
-    
-    
+
     public void infoRam() {
-        
+
 //        Pegando RAM total
         ram.getTotal();
 //        Pegando RAM em uso
         ram.getEmUso();
 //        Pegando RAM disponível
         ram.getDisponivel();
-        
     }
-    
-    public void infoProcessador(){
-        
+
+    public void infoProcessador() {
+
 //        Pegando o nome do Processador
         processador.getNome();
-//        Pegando o numero de cpus fisicas
-        processador.getNumeroCpusFisicas();
-//        Pegando o numero de cpus logicas
-        processador.getNumeroCpusLogicas();
+//        Pegando o identificador do processador
+        processador.getIdentificador();
+//        Pegando a frequência do processador
+        processador.getFrequencia();
+//        Pegando o uso do processador
+        processador.getUso();
     }
-    
+
     public void infoMemoriaDisco() {
-        
-//        Tamanho total do Disco
+
+//        Pegando o tamanho total do disco
         disco.getTamanhoTotal();
+//        Pegando o(s) disco(s) da máquina        
+        disco.getDiscos();
+//        Pegando a quantidade de discos da máquina        
+        disco.getQuantidadeDeDiscos();
+//        Pegando os volumes da máquina        
+        disco.getVolumes();
+//        Pegando a quantidade de volumes da máquina        
+        disco.getQuantidadeDeVolumes();
     }
-    
+
+//    @Override
+//    public String toString() {
+//        return "ServiceTeste{" + "ram=" + ram + "\n" + ", processador=" + processador + "\n" + ", disco=" + disco + '}';
+//    }
+
 }
