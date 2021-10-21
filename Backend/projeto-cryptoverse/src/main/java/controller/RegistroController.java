db26bc6120cd9e225192bbb0daae3af00c68cf91
Fabricio-Ajala <fabricio_ajala@hotmail.com>
2021-10-21 05:51:23 -0300

    Ajustando pastas API presentes no Git

---
+++ Backend/projeto-cryptoverse/src/main/java/controller/RegistroController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import services.ServiceTeste;
+
+public class RegistroController {
+
+    ServiceTeste service = new ServiceTeste();
+
+//    public void infoRam(Long tamanhoTotal){
+//        service.infoRam(tamanhoTotal);
+//    }
+
+}
