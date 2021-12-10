b265eb1315333301afa1df7379cabfcc3c9f5bfd
Zakeh1480 <zakeh0875@hotmail.com>
2021-12-10 13:44:15 -0300

    Ajuste login/cadastro

--- Backend/projeto-cryptoverse/src/main/java/services/Slack.java
+++ Backend/projeto-cryptoverse/src/main/java/services/Slack.java
@@ -1,4 +1,4 @@
-    package services;
+        package services;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,8 +10,8 @@ import org.json.JSONObject;
 public class Slack {
 
     private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String URL = "https://hooks.slack.com/services/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";
-    private static final String token = "xoxb-2803705969300-2801443846226-vr4R8vfpz0OsigR5bn6fuwUT";
+    private static final String URL = "https://hooks.slack.com/services/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";
+    private static final String token = "xoxb-2803705969300-2801443846226-0RkjagCAOB1QSOQo8MorxnqN";
     private static final String tokenBot = "xoxe.xoxp-1-Mi0yLTI4MDM3MDU5NjkzMDAtMjgwMTQzODAxMDI1OC0yODI1NzA2MTY1ODg5LTI4MTAxMzg5OTY3MDktNjU2MDU5MmRhOGEwYmFkMDgxYjU5YWViNjJhOWJmZjk0OGExZTM3YThhYzlkMjZjODBmOThmNDdmYTBlYjlhNg";
 
     public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
