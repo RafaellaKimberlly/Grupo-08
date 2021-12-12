cfefb10ae3124b1efbd69c09b881e3786b4b74b3
Zakeh1480 <zakeh0875@hotmail.com>
2021-12-11 21:09:19 -0300

    Slack webhook fixo

--- Backend/projeto-cryptoverse/src/main/java/services/Slack.java
+++ Backend/projeto-cryptoverse/src/main/java/services/Slack.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 public class Slack {
 
     private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String URL = "https://hooks.slack.com/services/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";
+    private static final String URL = "https://hooks.slack.com/services/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";
     private static final String token = "xoxb-2803705969300-2801443846226-0RkjagCAOB1QSOQo8MorxnqN";
     private static final String tokenBot = "xoxe.xoxp-1-Mi0yLTI4MDM3MDU5NjkzMDAtMjgwMTQzODAxMDI1OC0yODI1NzA2MTY1ODg5LTI4MTAxMzg5OTY3MDktNjU2MDU5MmRhOGEwYmFkMDgxYjU5YWViNjJhOWJmZjk0OGExZTM3YThhYzlkMjZjODBmOThmNDdmYTBlYjlhNg";
 
